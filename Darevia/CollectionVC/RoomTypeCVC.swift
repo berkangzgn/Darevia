f7654cbf9f2bdbf38b50f2d0ea0ff91af730d6a5
berkangzgn <bgezgin@protonmail.com>
2024-01-31 18:44:39 +0300

    RoomTypeCVC and RoomTypeTVC createed. Main page tableView added and controller codes written.

---
+++ Darevia/CollectionVC/RoomTypeCVC.swift
@@ -0,0 +1,17 @@
+//
+//  RoomTypeCVC.swift
+//  Darevia
+//
+//  Created by Berkan Gezgin on 31.01.2024.
+//
+
+import UIKit
+
+class RoomTypeCVC: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ Darevia/TableVC/RoomTypeTVC.swift
@@ -0,0 +1,23 @@
+//
+//  RoomTypeTVC.swift
+//  Darevia
+//
+//  Created by Berkan Gezgin on 31.01.2024.
+//
+
+import UIKit
+
+class RoomTypeTVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
