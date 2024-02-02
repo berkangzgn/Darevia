4d1a55273939761bc325efcd5115eb7371098c2a
berkangzgn <bgezgin@protonmail.com>
2024-02-02 23:52:11 +0300

    Comment collectionView created

---
+++ Darevia/CollectionVC/CommentCVC.swift
@@ -0,0 +1,17 @@
+//
+//  CommentCVC.swift
+//  Darevia
+//
+//  Created by Berkan Gezgin on 2.02.2024.
+//
+
+import UIKit
+
+class CommentCVC: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ Darevia/TableVC/CommentsTVC.swift
@@ -0,0 +1,23 @@
+//
+//  CommentsTVC.swift
+//  Darevia
+//
+//  Created by Berkan Gezgin on 2.02.2024.
+//
+
+import UIKit
+
+class CommentsTVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
