36c41432caec07f9ff3291c212bf052a41d590a6
berkangzgn <bgezgin@protonmail.com>
2024-02-04 04:55:54 +0300

    GameTypes view created.

---
+++ Darevia/CollectionVC/GamesCVC.swift
@@ -0,0 +1,17 @@
+//
+//  GamesCVC.swift
+//  Darevia
+//
+//  Created by Berkan Gezgin on 4.02.2024.
+//
+
+import UIKit
+
+class GamesCVC: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
