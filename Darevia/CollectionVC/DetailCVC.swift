03ad8a8bc6bc9e9fff2d1a293b003daee67b7e86
berkangzgn <bgezgin@protonmail.com>
2024-02-06 00:05:43 +0300

    DetailCollectionViewCell created. Some extensions added.

---
+++ Darevia/CollectionVC/DetailCVC.swift
@@ -0,0 +1,17 @@
+//
+//  DetailCVC.swift
+//  Darevia
+//
+//  Created by Berkan Gezgin on 5.02.2024.
+//
+
+import UIKit
+
+class DetailCVC: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
