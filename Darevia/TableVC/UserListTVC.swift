504a0e124be2e7bce9be97ce97173c01ee235aec
berkangzgn <bgezgin@protonmail.com>
2024-02-07 19:09:24 +0300

    Waiting romm created. Table cells implemented. Data updated.

---
+++ Darevia/TableVC/UserListTVC.swift
@@ -0,0 +1,23 @@
+//
+//  UserListTVC.swift
+//  Darevia
+//
+//  Created by Berkan Gezgin on 7.02.2024.
+//
+
+import UIKit
+
+class UserListTVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
